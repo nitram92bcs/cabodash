a3ba110b6d9ca68cc2afef36399315d1a67f31b5
Martin Urias <nitram92bcs@gmail.com>
2018-07-16 18:12:24 -0600

    Migraciones y rutas

--- database/migrations/2018_07_16_174912_create_nota_compras_table.php
+++ database/migrations/2018_07_16_174912_create_nota_compras_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
 
 class CreateNotaComprasTable extends Migration
 {
@@ -15,7 +16,15 @@ class CreateNotaComprasTable extends Migration
     {
         Schema::create('nota_compras', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+            $table->integer('proveedor_id')->unsigned()->nullable();
+            $table->string('compras');
+            $table->decimal('total', 9, 2);
+            $table->enum('status',['pendiente','parcial','pagado'])->default('pendiente');
+            $table->decimal('balance', 9, 2);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->default(new Expression('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('deleted_at')->nullable();
+            $table->foreign('proveedor_id')->references('id')->on('proveedores');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,3 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
