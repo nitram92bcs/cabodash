3ed488d054f570e778f3e8a2d63f82a5253c5af4
Martin Urias <nitram92bcs@gmail.com>
2019-04-29 16:30:45 -0600

    Codigo de Google Analytics

--- resources/views/layouts/freelance.blade.php
+++ resources/views/layouts/freelance.blade.php
@@ -46,6 +46,15 @@
         <![endif]-->
             <!--  Verificación de Google -->
         <meta name="google-site-verification" content="0Ay3R22Ss5gabc4fIELiLQCRqpe-7at5Kebyo44XSAY" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139276914-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-139276914-1');
+        </script>
+
         @yield('style')
     </head>
 
